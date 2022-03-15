419f8803dc325cebbf544103178d17bfbe7f2e16
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-15 16:44:56 +0100

    changes in sendLogData

--- src/features/LoginPage/sendLoginData.js
+++ src/features/LoginPage/sendLoginData.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 export const sendLoginData = async (user) => {
-    await axios.post('http://localhost:8080/login', {
+    const response = await axios.post('http://localhost:8080/login', {
         username: user.username,
         password: user.password,
-}).then(response => console.log(response.headers.authorization))
-.catch(error => console.error(error));
+    })
+
+    if (!response.ok) {
+        new Error(response.statusText);
+    }
+
+    return response.headers.authorization;
 } 
