dee7b3854198e06810bc54671bee0791f7a13d8e
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-15 17:01:17 +0100

    added saving token in local storage

--- src/features/LoginPage/loginSaga.js
+++ src/features/LoginPage/loginSaga.js
@@ -1,14 +1,15 @@
 import { call, select, takeEvery } from "redux-saga/effects";
 import { selectLogin } from "./loginSlice";
 import { sendLoginData } from "./sendLoginData";
+import { saveLoginDataInLocalStorage } from "./loginDataLocalStorage";
 
 function* sendLoginHandler() {
-
     try {
         const login = yield select(selectLogin);
         const token = yield call(sendLoginData, login);
+        yield call(saveLoginDataInLocalStorage, token);
     } catch (error) {
-        yield call(alert, error);
+        yield call(alert, "Brak połączenia z internetem lub serwer nie odpowiada!");
     }
 }
 
