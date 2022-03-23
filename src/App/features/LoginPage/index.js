ef0a6130a44d7624c5ec7e873d9bf3a43bd6db48
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-23 15:00:59 +0100

    added new header on login page

--- src/App/features/LoginPage/index.js
+++ src/App/features/LoginPage/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import Form from "./Form";
 import Main from "../../common/Main";
 import Section from "../../common/Section";
+import {Header} from "./styled";
 
 const LoginPage = () => {
     return (
         <Main>
-            <Section header={"Logowanie do social media"}></Section>
+            <Section header={<Header>social media</Header>}></Section>
             <Section body={<Form />}></Section>
         </Main>
     )
---
+++ src/App/features/LoginPage/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Header = styled.h2`
+    color: ${({theme}) => theme.color.blue};
+    font-size: 35px;
+`;
