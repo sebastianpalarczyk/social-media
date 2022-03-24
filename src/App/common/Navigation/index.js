b79c40884232b108ca1c5353788d4f00199029df
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-24 17:18:23 +0100

    changed icon of logo

--- src/App/common/Navigation/index.js
+++ src/App/common/Navigation/index.js
@@ -4,32 +4,32 @@ import { ReactComponent as HomepageIcon } from "../../images/homepage.svg";
 import { ReactComponent as Groups } from "../../images/groups.svg";
 import { ReactComponent as Watch } from "../../images/watch.svg";
 import { ReactComponent as MarketPlace } from "../../images/marketplace.svg";
-import {ReactComponent as Menu} from "../../images/menu.svg";
-import {ReactComponent as Flag} from "../../images/flag.svg";
-import {ReactComponent as Messages} from "../../images/messages.svg";
-import {ReactComponent as Notifications} from "../../images/notifications.svg";
-import {ReactComponent as Logo} from "../../images/logo.svg";
-import {ReactComponent as Account} from "../../images/account.svg";
+import { ReactComponent as Menu } from "../../images/menu.svg";
+import { ReactComponent as Flag } from "../../images/flag.svg";
+import { ReactComponent as Messages } from "../../images/messages.svg";
+import { ReactComponent as Notifications } from "../../images/notifications.svg";
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import { ReactComponent as Account } from "../../images/account.svg";
 
 const Navigation = () => {
   return (
     <NavigationContainer>
       <List>
         <Item>
-          <Link><Logo/></Link>
+          <Link><Logo /></Link>
         </Item>
         <Item>
-          <Link><HomepageIcon/></Link>
-          <Link><Flag/></Link>
-          <Link><Watch/></Link>
-          <Link><MarketPlace/></Link>
-          <Link><Groups/></Link>
+          <Link><HomepageIcon /></Link>
+          <Link><Flag /></Link>
+          <Link><Watch /></Link>
+          <Link><MarketPlace /></Link>
+          <Link><Groups /></Link>
         </Item>
         <Item>
-          <Link><Menu/></Link>
-          <Link><Messages/></Link>
-          <Link><Notifications/></Link>
-          <Link><Account/></Link>
+          <Link><Menu /></Link>
+          <Link><Messages /></Link>
+          <Link><Notifications /></Link>
+          <Link><Account /></Link>
         </Item>
       </List>
     </NavigationContainer>
